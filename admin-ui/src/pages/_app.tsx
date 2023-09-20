a8823b205166ab635d94b330230cd699b7a4944d
aabdg346 <aamare@blackdiamondgroup.com>
2023-09-20 10:01:26 -0600

    title change to hotel stations for admin

--- admin-ui/src/pages/_app.tsx
+++ admin-ui/src/pages/_app.tsx
@@ -69,7 +69,7 @@ class App extends React.Component<Props, State> {
           <link rel="shortcut icon" href="/admin/favicon-192.png" />
           <link rel="apple-touch-icon" href="/admin/favicon-192.png" />
           <link rel="apple-touch-startup-image" href="/admin/favicon-1024.png" />
-          <title>Seatsurfing</title>
+          <title>Hotel Stations</title>
         </Head>
         <Component {...pageProps} />
       </>
